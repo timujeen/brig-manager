2793f19f5438488c060fad1b400714f29169f036
timujeen <rtimurb@mail.ru>
2013-05-19 11:33:35 +0400

    added russian to db:seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,4 @@
+#encoding: utf-8
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
@@ -6,8 +7,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Country.delete_all
-Country.create!(title: 'Russia')
-Country.create!(title: 'China')
-Country.create!(title: 'Germany')
-
-Job.delete_all
+Country.create!(title: 'Россия')
+Country.create!(title: 'Китай')
+Country.create!(title: 'Германия')
